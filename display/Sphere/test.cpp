67d164adf3e9a8d3914d032639c602e5374f104d
Robert BuRnEr Schadek <realburner@gmx.de>
2010-03-02 01:38:43 +0100

    some minor changes

--- display/Sphere/test.cpp
+++ display/Sphere/test.cpp
@@ -6,24 +6,37 @@ void putPixelB(SDL_Surface* sur, int x, int y, double al) {
 	unsigned *pixel = (unsigned*)sur->pixels;
 	//std::cout<<"x "<<x<<" y "<<y<<" "<<al*255<<std::endl;
 	//std::cout<<"sur->h "<<sur->h<<" sur->w "<<sur->w<<std::endl;
-	pixel[(y*sur->h)+x] = (unsigned)(al*255);
+	unsigned a = 0;
+	//unsigned a = 255<<24;
+	//a += 255<<16;
+	//a+=255<<8;
+	if(al > 0.0) {
+		a+=(unsigned)(pow(2,12) * al);
+	} else {
+		a+=(unsigned)(pow(2,12)* (-al));
+	}
+	//std::cout<<a<<std::endl;
+	pixel[(y*sur->h)+x] = a;
 }
 
 int main() {
-	std::cout<<"foo";
+	std::cout<<"foo\n";
 	SDL_Surface* hello = NULL;
 	SDL_Init(SDL_INIT_EVERYTHING);
-	int x = 255;
-	int y = 255;
+	int x = 512;
+	int y = 512;
 	hello = SDL_SetVideoMode(x,y,32,SDL_SWSURFACE);
+	double steps = 1.0 / x;
 	for(int i = 0; i < x; i++) {
 		for(int j = 0; j < y; j++) {
-			//double w = fbm(1.0-(1.0/i), 1.0-(1.0/j), 0.0, 6);
-			double w = snoise(1.0000001-(1.0/i), 1.0-(1.0/j),0.0);
-			std::cout<<"w "<<w<<" "<<" i "<<1.000001-(1.0/i+1)<<" j "<<1.0-(1.0/j+1)<<std::endl;
+			//double w = fbm(i*steps,j*steps, 0.0, 15);
+			//double w = turbulence(i*steps,j*steps, 0.0, 5);
+			double w = noise(i*steps,j*steps, 0.0);
+			//double w = snoise(0.0000001+(1.0/(i+1)), 0.0+(1.0/(j+1)),0.0);
+			//std::cout<<"w "<<w<<" i "<<i*steps<<" j "<<j*steps<<std::endl;
 			putPixelB(hello, i, j, w);
 		}
 	}
 	SDL_Flip(hello);
-	SDL_Delay(2000);
+	SDL_Delay(5000);
 }	
